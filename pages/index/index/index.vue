b063592fa229390e3997accb29a57da8bc74d740
caoyao <caoyao@testbird.com>
2020-02-17 18:26:17 +0800

    d

--- pages/index/index/index.vue
+++ pages/index/index/index.vue
@@ -4,7 +4,7 @@
         @scroll="scroll"
         :style="{height: $store.state.is_pc ? '100%' : $store.state.innerHeight+'px'}"
     >
-        <div class="display_none head_position width_90 box_szing phone_width_100 phone_relative">
+        <div class="head_position width_90 box_szing phone_width_100 phone_relative">
             <img src="../../../static/img/logo.png" alt />
         </div>
         <div
@@ -22,7 +22,7 @@
             <phone-soft-ware ref="soft_ware"  :translate-persent="phone_soft_page_scroll()" :class="{transition_back: isTransitionBack(7)}"></phone-soft-ware>
         </div>
         <!-- 下面的导航 文字和border分开 简单点 border 作为整个-->
-        <div class="navigation_positoin width_70 display_none phone_none cursor" >
+        <div class="navigation_positoin width_70 phone_none cursor" >
             <div class="display_flex flex_jusify_space">
                 <div
                     v-for="(item,index) in navigation_arr"
